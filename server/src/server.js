bfa179612c85d7d016df941f1b43d008a7ceb926
Mathialagan23 <mathialagan823@gmail.com>
2026-02-17 23:19:24 +0530

    port binding prob solv

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/database.js";
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({ path: './src/config/config.env' });
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
 }
 
-import app from './app.js';
-import connectDB from './config/database.js';
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 connectDB();
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
-
